db11d65e533c243de3342d45ee20bd17cbd62ad5
Oscar Ramirez <oscarramirez@10-18-11-168.dynapool.wireless.nyu.edu>
2021-10-28 11:30:11 -0400

    testing for server db

--- db/db.py
+++ db/db.py
@@ -3,10 +3,20 @@ This file will manage interactions with our data store.
 At first, it will just contain stubs that return fake data.
 Gradually, we will fill in actual calls to our datastore.
 """
+import json
+ROOMS_DB = "/Users/oscarramirez/Desktop/sweJOL/db/rooms.json"
 
 
-def fetch_pets():
+def get_rooms():
     """
-    A function to return all pets in the data store.
+    A function to return all chat rooms.
     """
-    return {"tigers": 2, "lions": 3, "zebras": 1}
+    try:
+        with open(ROOMS_DB) as file:
+            return json.loads(file.read())
+    except FileNotFoundError:
+        return None
+    '''
+    return {"Software Engineering": {"num_users": 90},
+                "AI": {"num_users": 90, "decr": "Discuss AI"}, }
+                '''
