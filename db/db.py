0c5d7298146c3a7931545d55bf571e707c3f372b
Oscar Ramirez <oscarramirez@10-21-215-153.dynapool.wireless.nyu.edu>
2021-10-28 15:16:29 -0400

    need to add environment variables

--- db/db.py
+++ db/db.py
@@ -4,7 +4,13 @@ At first, it will just contain stubs that return fake data.
 Gradually, we will fill in actual calls to our datastore.
 """
 import json
-ROOMS_DB = "/Users/oscarramirez/Desktop/sweJOL/db/rooms.json"
+# remove comment from 3 below and comment out line 13
+# import os
+
+# DEMO_HOME = os.environ["DEMO_HOME"]
+
+# ROOMS_DB = f"{DEMO_HOME}/db/rooms.json"
+ROOMS_DB = "../db/rooms.json"
 
 
 def get_rooms():
