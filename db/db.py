02824f66813ac57bbee76819ceb69a727aa9cce5
Oscar Ramirez <oscarramirez@10-21-6-115.dynapool.wireless.nyu.edu>
2021-11-04 15:02:45 -0400

    testing travis DEMO_HOME

--- db/db.py
+++ db/db.py
@@ -5,12 +5,13 @@ Gradually, we will fill in actual calls to our datastore.
 """
 import json
 # remove comment from 3 below and comment out line 13
-# import os
+import os
 
-# DEMO_HOME = os.environ["DEMO_HOME"]
+DEMO_HOME = os.environ["DEMO_HOME"]
 
-# ROOMS_DB = f"{DEMO_HOME}/db/rooms.json"
-ROOMS_DB = "../db/rooms.json"
+ROOMS_DB = f"{DEMO_HOME}/db/rooms.json"
+
+# ROOMS_DB = "../db/rooms.json"
 
 
 def get_rooms():
