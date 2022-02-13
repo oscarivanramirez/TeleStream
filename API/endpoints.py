8db9f4794524311427d04e3c503726887cc4befa
oscarivanramirez <oir209@nyu.edu>
2022-02-12 20:33:15 -0500

    testing for heroku

--- API/endpoints.py
+++ API/endpoints.py
@@ -113,20 +113,6 @@ class CreateMessage(Resource):
             return f"{message} added."
 
 
-@api.route('/bye')
-class ByeWorld(Resource):
-    """
-    The purpose of the HelloWorld class is to have a simple test to see if the
-    app is working at all.
-    """
-    def get(self):
-        """
-        A trivial endpoint to see if the server is running.
-        It just answers with "hello world."
-        """
-        return {'Bye': WORLD}
-
-
 @api.route('/endpoints')
 class Endpoints(Resource):
     """
